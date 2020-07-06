c59794b95b6bae57e40bb2e7aba203eacca29d25
Ramon Conrado <ramonconrado94@gmail.com>
2020-07-06 00:00:43 -0300

    retirando auth'

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -8,7 +8,7 @@ const authService = require('../services/auth-service');
 router.get('/',  authService.authorize, controller.get);
 router.get('/:slug',  authService.authorize, controller.getBySlug);
 router.get('/admin/:id',  authService.isAdmin, controller.getById);
-router.get('/tags/:tag',  authService.authorize, controller.getByTag);
+router.get('/tags/:tag', controller.getByTag);
 router.post('/', authService.authorize, controller.post);
 router.put('/:id',  authService.authorize, controller.put);
 router.delete('/',  authService.authorize, controller.delete);
